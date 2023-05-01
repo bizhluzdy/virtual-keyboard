e78463374e02c8305d5e862caa2215629bac50e4
bizhluzdy <murdrum22@gmail.com>
2023-05-01 09:45:58 +0200

    fix: fixed eslint
    Added eslint-config, eslint-plugins, eslint-airbnb. Also added new rules in eslint config, added new extends.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,18 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": "eslint:recommended",
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
-        "semi": ["error", "always"],
-        "quotes": ["error", "double"]
-    }
-}
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["airbnb", "airbnb/hooks", "eslint:recommended"],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    semi: ["error", "always"],
+    quotes: ["error", "double"],
+    "linebreak-style": ["error", "windows"],
+  },
+};
